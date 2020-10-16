c5ef13fc45231353aeb7e0ff71ae044f378e5d5a
PepsUDP <70279893+PepsUDP@users.noreply.github.com>
2020-10-16 14:11:16 -0300

    Update 3DES_ECB.py
    Comentarios

--- 3DES_ECB.py
+++ 3DES_ECB.py
@@ -1,67 +1,68 @@
-from base64 import b64encode
-from Crypto.Cipher import DES3
-from Crypto.Util.Padding import pad
-from Crypto.Random import get_random_bytes
-import webbrowser
+// ==UserScript==
+// @name         3DES_ECB
+// @namespace    https://github.com/PepsUDP/Tarea3Cripto/blob/main/3DES_ECB.js
+// @version      2.2
+// @description  Probando github pages
+// @author       José Durán
+// @downloadURL  https://github.com/PepsUDP/Tarea3Cripto/blob/main/3DES_ECB.js
+// @match        https://pepsudp.github.io/Tarea3Cripto/3DES_ECB.html
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/tripledes.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/core-min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/mode-ecb.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/enc-base64.js
+// ==/UserScript==
 
-data = b'password'
-while True:
-    try:
-        #Se obtiene llave aleatoria (en concordancia con los requisitos de 3DES
-        #en bytes "y" hexadecimal
-        key = DES3.adjust_key_parity(get_random_bytes(24))
-        print(key)
-        #Llave en ASCII para que sea de fácil uso en JavaScript
-        keyASCII = b64encode(key).decode('utf-8')
-        print(keyASCII)
-        break
-    except ValueError:
-        pass
+//Función para pasar de hexadecimal a ASCII
+function hex2a(hex) {
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+};
 
-#Se establece el algoritmo y modo a utilizar
-cipher = DES3.new(key, DES3.MODE_ECB)
-#Se agrega padding al mensaje y se cifra con los parámetros establecidos 
-#(Al ser ECB no necesitamos salt, IV, etc.). Se obtiene el mensaje cifrado en bytes.
-ct_bytes = cipher.encrypt(pad(data,DES3.block_size))
-#El mensaje cifrado se transforma a base64 para poder integrar con TamperMonkey
-ct = b64encode(ct_bytes).decode('utf-8')
-print(ct_bytes)
-print(ct)
+(function() {
+    'use strict';
+    //Al igual que en la aplicación de 3DES modo ECB en python, las únicas variables a utilizar en el cifrado y descifrado
+    //de esta tarea serán los datos a manipular y el modo, pero al ser este último constante y el mensaje depende de lo que
+    //se pase a través de python no existen variables modificables (obligatorias) para el funcionamiento de este cifrado.
 
-#Se abre el contacto con el archivo .html
-f = open('3DES_ECB.html','w')
-#Se escribe en el archivo
-mensaje0 = """
-<html>
-<head></head>
-<body>
-<p>Mensaje Secreto</p>
-<div class="key" id=\""""
-f.write(mensaje0)
-f.write(keyASCII)
-mensaje1 = """\">
-<p></p>
-</div>
-<div class="pad" id=\""""
-f.write(mensaje1)
-#Pkcs7 es el padding default para esta librería y algoritmo por lo que no es necesario enviarlo
-#pero sigue siendo una variable al momento de cifrar, por lo que no está de más
-f.write("pkcs7")
-mensaje2 = """\">
-<p></p>
-</div>
-<div class="3DES" id=\""""
-f.write(mensaje2)
-f.write(ct)
-mensaje3 = """">
-<p></p>
-</div>
-</body>
-</html>"""
-f.write(mensaje3)
+    //Se agarra el mensaje cifrado, se convierte a arreglo para que sea manejable por CryptoJS, se obtiene el padding,
+    //y por último la llave para luego pasarla a base64, nuevamente para mejor manejo por parte de CryptoJS
+    var encrypted = document.getElementsByClassName("3DES")["0"].getAttribute("id");
+    var ObjectArray = CryptoJS.enc.Base64.parse(encrypted);
+    var padding = document.getElementsByClassName("pad")["0"].getAttribute("id");
+    var key = document.getElementsByClassName("key")["0"].getAttribute("id");
+    var keyArray = CryptoJS.enc.Base64.parse(key);
 
-#Se cierra el canal de comunicación
-f.close()
+    //Código solamente relevante para cuando el mensaje cifrado es "password", el objetivo es que para el ejemplo principal
+    //se logre observar que el mensaje a decifrar es correcto.
+    var testencrypt = CryptoJS.TripleDES.encrypt("password", keyArray, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+    }).toString();
+    var testdecrypt = CryptoJS.TripleDES.decrypt(testencrypt, keyArray, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+    }).toString();
 
-#Comando para abrir el archivo .html en una nueva pestaña
-webbrowser.open_new_tab('3DES_ECB.html')
+    //se aplica la desencriptación del mensaje cifrado con el modo ECB y el padding Pkcs7 (default para Pycryptodome),
+    //luego se pasan ambos mensajes de cifrados a ASCII / texto plano.
+    var decrypted = CryptoJS.TripleDES.decrypt(encrypted, keyArray, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+    }).toString();
+    var decryptA = hex2a(decrypted);
+    var testdecryptA = hex2a(testdecrypt);
+
+    //Se inserta el mensaje en texto plano descifrado dentro del div 3DES.
+    document.getElementsByClassName("3DES")["0"].innerText = decryptA;
+    //Distintos console.logs() para observar el proceso de transformación de la información.
+    console.log("Encriptado:",encrypted);
+    console.log("EncriptadoArreglo:",ObjectArray);
+    console.log("Desencriptado:",decrypted);
+    console.log("DesencriptadoASCII:",decryptA);
+    console.log("Llave:",key);
+    console.log("LlaveArreglo:",keyArray);
+    console.log("EncriptadoTest:",testencrypt);
+    console.log("DesencriptadoTest:",testdecrypt);
+    console.log("DesencriptadoTestASCII:",decryptA);
+})();
